0c75d194fcb36f014574c2ba320e54115c5e3809
Alfonso Moceri <156968542+almoceri@users.noreply.github.com>
2024-04-06 14:52:33 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,140 @@
+var video = document.getElementById("player1");
+var vol;
+var autopl = video.autoplay;
+var loops = video.loop;
+var speed = video.playbackRate;
+
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	autopl = false;
+	loops = false;
+	console.log(autopl);
+	console.log(loops);
+
+
+
+
+});
+
+
+
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	vol = video.volume;
+	console.log(vol);
+
+});
+
+
+
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+
+});
+
+
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Video");
+	console.log(speed);
+
+	video.playbackRate = speed*.9;
+	speed = speed*.9;
+
+	console.log(speed);
+
+
+});
+
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed up Video");
+	console.log(speed);
+
+	video.playbackRate = speed*1.1;
+	speed = speed*1.1;
+
+	console.log(speed);
+
+
+});
+
+var time;
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Video");
+	time = video.currentTime;
+	console.log(time);
+
+	if ((time + 5) > video.duration) {
+		video.currentTime = 0;
+		time = video.currentTime;
+	}
+
+	else {
+		video.currentTime = time + 5;
+		time = video.currentTime;
+	}
+	
+	
+
+	console.log(time);
+
+
+});
+
+
+var mute;
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("mute Video");
+	mute = video.muted;
+	if(mute){
+		video.muted = false;
+	}
+
+	else {
+		video.muted = true;
+	}
+
+	mute = video.muted;
+
+
+	console.log(mute);
+
+
+});
+
+
+var slide;
+document.querySelector("#slider").addEventListener("input", function() {
+	slide = document.getElementById("slider").value/100; 
+	video.volume = slide;
+	console.log(slide);
+
+
+
+});
+
+
+
+document.querySelector("#vintage").addEventListener("click", function() {
+
+	video.className = "video oldSchool"
+
+
+
+});
+
+
+document.querySelector("#orig").addEventListener("click", function() {
+
+	video.className = "video"
+
+
+
+});
